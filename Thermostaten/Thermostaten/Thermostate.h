d73aa012ef3f809b6698ee88f3527cfba5da198f
paddy3 <paddytree3@gmail.com>
2014-10-21 14:31:33 +0200

    Kein Thermostat erstellt: leer

---
+++ Thermostaten/Thermostaten/Thermostate.h
@@ -0,0 +1,13 @@
+#ifndef Monomer_H
+#define Monomer_H
+
+#include<cmath>
+
+#include "Polymer.h"
+
+void Thermos_none(Polymer polymer, double time) {
+// kein Thermostat
+	
+} //Thermos_none
+
+#endif
