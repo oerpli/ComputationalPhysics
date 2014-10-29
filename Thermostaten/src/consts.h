2fd2e90d671bbc8f79f10d7ef8d252f89352b84c
paddy3 <paddytree3@gmail.com>
2014-10-29 02:21:18 +0100

    klingendere Namen für Referenz

--- Thermostaten/src/consts.h
+++ Thermostaten/src/consts.h
@@ -6,6 +6,7 @@ namespace consts{
 	const double ref_mass=3.3474E-27; 	//in kg...	über molare Masse
 	const double ref_temp=20;						//in °K
 	const double ref_energy=1.2787E-19;		//in J...		über Epot
+	
 	const double ref_time=5.401E-13;		//in s...
 	
 	const double ref_speed=1.61799E-4; 
