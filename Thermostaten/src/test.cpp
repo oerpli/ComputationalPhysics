4edb33f9a4c3aa557da88bb593d19ec7a96aa8aa
paddy3 <paddytree3@gmail.com>
2014-10-29 02:26:52 +0100

    Oszillation in Ekin kann beobachtet werden

--- Thermostaten/src/test.cpp
+++ Thermostaten/src/test.cpp
@@ -3,6 +3,9 @@
 #include "Thermostat_None.h"
 #include "Nose_Hoover.h"
 
+#include "consts.h"
+using namespace consts;
+
 #include<iostream>
 #include<stdlib.h>
 
@@ -11,23 +14,24 @@ using namespace std;
 int main() {
 
 	cout << "Hello World" << endl;
-
-	Polymer p_a(10);
+	Polymer p_a(10,1);
 	cout << "T vor skalierung: " << p_a.ekin*2.0/(p_a.monomers.size() -1) << endl; 
 
 	p_a.update_ekin(); 
 	cout << "T nach skalierung: " << p_a.ekin*2.0/(p_a.monomers.size() -1) << endl; 
 
-	Thermostat_None thermostat(p_a, 0.001); 
-	//Nose_Hoover thermostat(p_a, 1.0, 0.1);
+	Thermostat_None thermostat(p_a, 2E-3);
 
-	cout << p_a << endl;
-	//cout << "calculation no thermostat" << endl;
-
-	//cout << p_a << endl;
-	//cout << "calculation N-H" << endl;
-	//thermostat.Propagate(1.);
-	//cout << p_a << endl;
+	for (int i=0;i<2E4;i++) {
+		if (! (i%(int)5E3) ) {
+			cout << endl << p_a;
+			cout << "Ekin: " << p_a.update_ekin() << endl; 
+		}
+		thermostat.propagate();
+	}
+		
+	cout << endl << p_a;
+	cout << "Ekin: " << p_a.update_ekin() << endl; 
 
 	return 0;
 }
