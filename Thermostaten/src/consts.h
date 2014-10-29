afb51073198340758df4d11e063243f2b1ac4aab
paddy3 <paddytree3@gmail.com>
2014-10-29 20:11:34 +0100

    Längenreferenz auf 2.76A geändert

--- Thermostaten/src/consts.h
+++ Thermostaten/src/consts.h
@@ -2,16 +2,15 @@
 #define CONSTS_H
 
 namespace consts{
-	const double ref_length = 3.3380976E-9;	//in m... 	über molare Dichte
+	const double ref_length = 2.76E-10;	//in m... 	über molare Dichte
 	const double ref_mass = 3.3474E-27; 	//in kg...	über molare Masse
 	const double ref_temp = 20;						//in °K
-	const double ref_energy = 1.2787E-19;		//in J...		über Epot
+	const double ref_energy = 8.741E-22;		//in J...		über Epot
 
 	const double ref_time = 5.401E-13;		//in s...
 
-	const double ref_speed = 1.61799E-4;
-	const double ref_hbar = 1.52698E-3;
-	const double ref_k = 2.1594588E-3;
+	const double ref_hbar = 0.223378278;
+	const double ref_k = 0.31590207;
 }
 
 #endif
