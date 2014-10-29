125786a73d03747db1f5378711a7a314bb47219a
paddy3 <paddytree3@gmail.com>
2014-10-29 01:08:30 +0100

    namespace für Referenzen und Konstanten

---
+++ Thermostaten/src/consts.h
@@ -0,0 +1,15 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+namespace consts{
+	const double ref_l=3.3380976E-9;	//in m... 	über molare Dichte
+	const double ref_m=3.3474E-27; 	//in kg...	über molare Masse
+	const double ref_t=20;						//in °K
+	const double ref_e=1.2787E-19;		//in J...		über Epot
+	const double ref_s=5.401E-13;		//in s...
+	
+	const double ref_hbar=1.52698E-3;	//in ES
+	const double ref_k=2.1594588E-3;		//in E/T
+}
+
+#endif
