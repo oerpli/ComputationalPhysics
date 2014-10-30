986b7e4d25bd36b1ca48a08f17e00ac2a0ce898f
Abraham Hinteregger <oerpli@hotmail.com>
2014-10-30 13:41:53 +0100

    numeric entfernt und mehr schleifen angepasst

--- Thermostaten/src/Polymer.cpp
+++ Thermostaten/src/Polymer.cpp
@@ -5,36 +5,27 @@
 #include<stdlib.h>
 #include<vector>
 #include<algorithm>
-#include<numeric>
 #include "consts.h"
 using namespace consts;
 using namespace std;
 
 void Polymer::initiate_monomers_random() {
 	double av_velocity = 0.0;
-	for (auto& m : monomers)
-		av_velocity += m.velocity = drand48() - 0.5;
-
+	for (auto& m : monomers) {
+		m.velocity = drand48() - 0.5;
+		av_velocity += m.velocity;
+	}
 	av_velocity /= monomers.size();
-
-	for (auto& m : monomers) {
-		m.velocity -= av_velocity;
-	}
-
+	for (auto& m : monomers) m.velocity -= av_velocity;
 	update_ekin();
-
 	double scale_factor = sqrt(temp()*monomers.size() / ekin*0.5*ref_k);
-	for (auto& m : monomers) {
-		m.velocity = m.velocity*scale_factor;
-	}
+	for (auto& m : monomers) m.velocity *= scale_factor;
 }
 
 void Polymer::initiate_monomers_one() { //erstes Monomer großteil der Energie
 	double max_vel = sqrt((monomers.size() - 1)*ref_k*temp() / monomer_mass);
 	double speed = -max_vel / (monomers.size() - 1);
-	for (auto& m : monomers){
-		m.velocity = speed;
-	}
+	for (auto& m : monomers) m.velocity = speed;
 	monomers[0].velocity = max_vel;
 }
 
@@ -42,7 +33,6 @@ Polymer::Polymer(int length, double temperature) : epot(0) {
 	monomers = std::vector<Monomer>(length, Monomer(0.0, 0.));
 	monomer_mass = 1. / length;
 	temp(temperature);
-        
 	initiate_monomers_one();
 }
 
@@ -67,9 +57,7 @@ double Polymer::force(double r) { return -r*feder_konst; }
 
 void Polymer::update_forces() {
 	//wahrscheinlich durch Optimierung der nächsten Schleife unnötig
-	for (auto& m : monomers){
-		m.force = 0;
-	}
+	for (auto& m : monomers) m.force = 0;
 	unsigned int size = monomers.size();
 	for (unsigned int i = 1; i <= size; i++) {
 		double force_buf = force(monomers[i - 1] - monomers[i % size]);
@@ -80,23 +68,16 @@ void Polymer::update_forces() {
 
 double Polymer::update_ekin(){
 	ekin = 0.0;
-	for (auto& m : monomers) {
-		ekin += m.velocity*m.velocity; //mass = 1 
-	}
+	for (auto& m : monomers) ekin += m.velocity*m.velocity; //mass = 1 
 	ekin *= monomer_mass*0.5;
 	return ekin;
 }
 
 double Polymer::calculate_temp() const {
 	double av_velocity = 0, av_energy = 0;
-
-	for (auto& m : monomers)
-		av_velocity += m.velocity;
+	for (auto& m : monomers) av_velocity += m.velocity;
 	av_velocity /= monomers.size();
-
-	for (auto& m : monomers)
-		av_energy += pow(m.velocity - av_velocity, 2);
+	for (auto& m : monomers) av_energy += pow(m.velocity - av_velocity, 2);
 	av_energy *= monomer_mass / 2 / monomers.size();
-
 	return av_energy / (0.5*ref_k);
 }
