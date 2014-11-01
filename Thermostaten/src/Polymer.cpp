5f4b60dc9e1654c9e45ee8c85a1fc74ded1261fa
paddy3 <paddytree3@gmail.com>
2014-11-01 22:41:11 +0100

    Optimieren update_force formatierung

--- Thermostaten/src/Polymer.cpp
+++ Thermostaten/src/Polymer.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 void Polymer::initiate_monomers_random() {
 	double av_velocity = 0.0;
+        
 	for (auto& m : monomers) {
 		m.velocity = drand48() - 0.5;
 		av_velocity += m.velocity;
@@ -25,6 +26,7 @@ void Polymer::initiate_monomers_random() {
 void Polymer::initiate_monomers_one() { //erstes Monomer großteil der Energie
 	double max_vel = sqrt((monomers.size() - 1)*ref_k*temp() / monomer_mass);
 	double speed = -max_vel / (monomers.size() - 1);
+        
 	for (auto& m : monomers) m.velocity = speed;
 	monomers[0].velocity = max_vel;
 }
@@ -56,25 +58,30 @@ std::ostream & Polymer::print(std::ostream &os) const {
 double Polymer::force(double r) { return -r*feder_konst; }
 
 void Polymer::update_forces() {
-	//wahrscheinlich durch Optimierung der nächsten Schleife unnötig
-	for (auto& m : monomers) m.force = 0;
-	unsigned int size = monomers.size();
-	for (unsigned int i = 1; i <= size; i++) {
-		double force_buf = force(monomers[i - 1] - monomers[i % size]);
-		monomers[i - 1].force += force_buf; //bin mir da mit dem Vorzeichen nicht sicher!
-		monomers[i % size].force -= force_buf;
-	}
+        double force_buf = 0;
+        auto mi=monomers.begin(), mj=monomers.begin();
+        mj->force=0;
+
+        for (mi=monomers.begin(), mj=mi, ++mj ; mi!=monomers.end() ; ++mi, ++mj) {
+                if ( mj==monomers.end() ) mj = monomers.begin();
+                else mj->force=0;
+                
+                force_buf = force(*mi - *mj);
+                mi->force += force_buf;
+                mj->force -= force_buf;
+        }
 }
 
 double Polymer::update_ekin(){
 	ekin = 0.0;
-	for (auto& m : monomers) ekin += m.velocity*m.velocity; //mass = 1 
+	for (auto& m : monomers) ekin += m.velocity*m.velocity;
 	ekin *= monomer_mass*0.5;
 	return ekin;
 }
 
 double Polymer::calculate_temp() const {
 	double av_velocity = 0, av_energy = 0;
+        
 	for (auto& m : monomers) av_velocity += m.velocity;
 	av_velocity /= monomers.size();
 	for (auto& m : monomers) av_energy += pow(m.velocity - av_velocity, 2);
