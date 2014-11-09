c6f36c3b60ef8a1d6e40c8d76a0269cc6f581714
Abraham Hinteregger <oerpli@hotmail.com>
2014-11-09 02:05:41 +0100

    temp -> target_temperature

--- Thermostaten/src/test.cpp
+++ Thermostaten/src/test.cpp
@@ -20,7 +20,7 @@ int main() {
 	p_a.update_ekin();
 	cout << "T nach skalierung: " << p_a.calculate_temp() << endl;
 	double step = 1E-15 / ref_time;
-	double q = p_a.monomers.size()*p_a.temp()*ref_time / 1E-13;
+	double q = p_a.monomers.size()*p_a.target_temperature()*ref_time / 1E-13;
 	cout << "q = " << q << endl;
 	//Thermostat_None thermostat(p_a, 1E-15 / ref_time);
 	Nose_Hoover thermostat(p_a, q, step);
