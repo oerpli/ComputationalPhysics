c447458a7868b56c3a06d86bdefe59458ff61989
Abraham Hinteregger <oerpli@hotmail.com>
2014-11-09 02:20:32 +0100

    functions.cpp

--- Thermostaten/src/Functions.h
+++ Thermostaten/src/Functions.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Thermostate.h"
 #include "Polymer.h" //redundant Thermostat
 #include "Monomer.h" //redundant Polymer
 
@@ -9,14 +8,7 @@
 
 using namespace std;
 
-ostream& operator<<(ostream& os, const Monomer & mono) {
-	print_m(mono, os);
-	return os;
-}
-
-ostream& operator<<(ostream& os, const Polymer & poly) {
-	poly.print(os);
-	return os;
-}
+ostream& operator<<(ostream& os, const Monomer & mono);
+ostream& operator<<(ostream& os, const Polymer & poly);
 
 
