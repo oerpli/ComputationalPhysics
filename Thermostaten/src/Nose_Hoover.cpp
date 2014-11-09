8d7f9995ca6fdfd7252be464797578776d9d4755
paddy3 <paddytree3@gmail.com>
2014-11-09 03:18:20 +0100

    Anpassung an Thermostat Klasse

--- Thermostaten/src/Nose_Hoover.cpp
+++ Thermostaten/src/Nose_Hoover.cpp
@@ -1,30 +1,42 @@
 #include "Nose_Hoover.h"
+#include <cmath>
 
-Nose_Hoover::Nose_Hoover(Polymer &a_polymer, double a_q, double a_step) :
-poly(a_polymer),
-q(a_q),
-step(a_step),
-stepsq(a_step*a_step) {
-	gkT = (poly.monomers.size() - 1.0)*poly.temp();
-	eta = 0.0;
-	poly.update_ekin();
-	poly.update_forces();
+Nose_Hoover::Nose_Hoover(Polymer &poly, double delta_time, double q) :
+Thermostat(poly,delta_time),
+m_q(q),
+m_eta(0.0)
+{
+	update_temp();
+	dtime(delta_time);
+	m_poly.update_ekin();
+	m_poly.update_forces();
 }
 
 Nose_Hoover::~Nose_Hoover() {}
 
+double Nose_Hoover::update_temp() {
+	m_gkT = (m_poly.monomers.size() - 1.0)*m_poly.temp();
+return m_gkT;	
+}
+
+double Nose_Hoover::dtime(double delta_time) { 
+	Thermostat::dtime(delta_time);
+	m_dtimesq=pow(m_dtime,2);
+	return m_dtime;
+}
+
 void Nose_Hoover::propagate() {
-	for (auto& m : poly.monomers){
-		m.position += step*m.velocity + stepsq*.5*((m.force / poly.monomer_mass) - eta*m.velocity);
-		m.velocity += step*0.5*((m.force / poly.monomer_mass) - eta*m.velocity);
+	for (auto& m : m_poly.monomers){
+		m.position += m_dtime*m.velocity + m_dtimesq*.5*((m.force / m_poly.monomer_mass) - m_eta*m.velocity);
+		m.velocity += m_dtime*0.5*((m.force / m_poly.monomer_mass) - m_eta*m.velocity);
 	}
 
-	eta += step*(poly.ekin - gkT*0.5) / q;
-	poly.update_ekin();
-	eta += step*(poly.ekin - gkT*0.5) / q;
+	m_eta += m_dtime*(m_poly.ekin - m_gkT*0.5) / m_q;
+	m_poly.update_ekin();
+	m_eta += m_dtime*(m_poly.ekin - m_gkT*0.5) / m_q;
 
-	poly.update_forces();
-	for (auto& m : poly.monomers) m.velocity = 2.0 * (m.velocity + (step*0.5*m.force / poly.monomer_mass)) / (2.0 + step*eta);
-	poly.update_ekin();
+	m_poly.update_forces();
+	for (auto& m : m_poly.monomers) m.velocity = 2.0 * (m.velocity + (m_dtime*0.5*m.force / m_poly.monomer_mass)) / (2.0 + m_dtime*m_eta);
+	m_poly.update_ekin();
 }
 
--- Thermostaten/src/Nose_Hoover.h
+++ Thermostaten/src/Nose_Hoover.h
@@ -1,23 +1,25 @@
 #ifndef Nose_Hoover_H
 #define Nose_Hoover_H
 
+#include "Thermostat.h"
+
 #include<cmath>
 #include "Polymer.h"
 
-class Nose_Hoover {
+class Nose_Hoover : Thermostat {
 
 private:
-	Polymer& poly;
-	double q;
-	double step;
-	double stepsq;
-	double gkT;
-	double eta;
+	double m_q;
+	double m_dtimesq;
+	double m_gkT;
+	double m_eta;
 
 public:
-	Nose_Hoover(Polymer&, double, double);
+	Nose_Hoover(Polymer& poly , double delta_time, double q);
 	~Nose_Hoover();
 
+	double	dtime(double new_dtime);
+	double update_temp();
 	void propagate();
 };
 #endif 
