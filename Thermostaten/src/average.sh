e6fe2e0b865ddb32e4c1cbb883281843e1e19e26
paddy3 <paddytree3@gmail.com>
2014-11-11 04:05:47 +0100

    Berechne Mittelwert einer Spalte Ubuntu only!

---
+++ Thermostaten/src/average.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/ksh
+
+awk '
+	BEGIN {
+		col="'$2'"+0;
+		min=max=$col+0;
+	}
+
+	{j++; val=$col+0;
+		av+=val;
+		
+		if (min > val)
+			min = val;
+		else if (max < val)
+			max = val;
+	}
+
+	END {
+		printf "%8g %8g %8g %8g\n",j,av/j,min,max;
+	} ' $1
+		
