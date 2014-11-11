aa93550375f0db232a6a9ea7ff904733cd9b4233
paddy3 <paddytree3@gmail.com>
2014-11-11 05:39:09 +0100

    Funktioniert soweit

--- Thermostaten/src/test_Thermostat.cpp
+++ Thermostaten/src/test_Thermostat.cpp
@@ -3,6 +3,7 @@
 #include "Lowe_Andersen.h"
 #include "Gaussian.h"
 #include "Nose_Hoover.h"
+#include "Thermostat_None.h"
 
 #include "consts.h"
 using namespace consts;
@@ -26,53 +27,73 @@ double set_param(double def, char *array[], int length, int pos) {
 	return def;
 }
 
+ostream& operator<<(ostream& os, const Polymer & poly) {
+	poly.print(os);
+	return os;
+}
 
+/* ######################### */
 int main(int argc, char* argv[]) {
 	double a_param[]{4, 20, 1E-15, 1E6};  //default für p,Temp,dtime,runs
-	int			i_para{};
+	int			i_para{1};
 	
-	for (i_para=0; i_para<min(4,argc-1); ++i_para) {
-		if ( is_number(argv[i_para+1]) ) a_param[i_para]=stod(argv[i_para+1]);
+	for (i_para=1; i_para<min(5,argc); ++i_para) {
+		if ( is_number(argv[i_para]) ) a_param[i_para-1]=stod(argv[i_para]);
 		else break;
 	}
 	
 	while ( i_para < argc - 1 && is_number( argv[i_para] ) ) ++i_para;
 	
-cout << argv[i_para] << endl;
+//cout << argv[i_para] << endl;
 	
 	a_param[2] /= ref_time;
 	Polymer poly{static_cast<unsigned> (a_param[0]), a_param[1]};
+	poly.initiate_monomers_random();
 	
+	// Initialisieren aller Thermostate
+	double nu{ set_param( 1./a_param[2]/a_param[0], argv, argc, i_para+1 ) };
+	Andersen andersen_therm{poly, a_param[2], nu};
+	Lowe_Andersen lowe_andersen_therm{poly, a_param[2], nu};
+	
+	Gaussian gaussian_therm{poly, a_param[2]};
 
-
-	if ( strcmp( argv[i_para] , "Andersen" ) == 0 ) {
-		double nu{ set_param( .1, argv, argc, i_para+1 ) };
-		Andersen thermostat{poly, a_param[2], nu};
+	double q_def{poly.monomers.size()*poly.temp()*ref_time / 1E-14};
+	double q{set_param (q_def, argv, argc, i_para+1) };
+	Nose_Hoover nose_hoover_therm{poly, a_param[2], q};
+	
+	Thermostat_None none_therm{poly, a_param[2]};
+	
+	// Auswählen des Thermostats
+	Thermostat *thermostat{};
+	if ( argc > 1 && i_para < argc ) {
+		if ( strcmp( argv[i_para] , "Andersen" ) == 0 ) {
+			thermostat = & andersen_therm;
+		}
+		else if ( strcmp( argv[i_para] , "Lowe_Andersen" ) == 0 ) {
+			thermostat = & lowe_andersen_therm;
+		}
+		else if ( strcmp( argv[i_para] , "Gaussian" ) == 0 ) {
+			thermostat = & gaussian_therm;
+		}
+		else if ( strcmp( argv[i_para] , "Nose_Hoover" ) == 0 ) {
+			thermostat = & nose_hoover_therm;
+		}
+		else {
+			thermostat = & none_therm;
+		}
 	}
-	else if ( strcmp( argv[i_para] , "Lowe_Andersen" ) == 0 ) {
-		double nu{ set_param( .1, argv, argc, i_para+1 ) };
-		Lowe_Andersen thermostat{poly, a_param[2], nu};
+  else {
+		thermostat = & none_therm;
 	}
-	else if ( strcmp( argv[i_para] , "Gaussian" ) == 0 ) {
-		Gaussian thermostat{poly, a_param[2]};
-	}
-	else if ( strcmp( argv[i_para] , "Nose_Hoover" ) == 0 ) {
-		double q_def{poly.monomers.size()*poly.temp()*ref_time / 1E-14};
-		double q{set_param (q_def, argv, argc, i_para+1) };
-		Nose_Hoover thermostat{poly, a_param[2], q};
-	}
-  else { 
-		cout << "Thermostat_None" << endl;
-		return 1;
-	}
-
+	
 	for (int i = 0; i < a_param[3]; i++) {
-		if (!(i % (int)1E5)) {
+		if ( !( i % (int)(a_param[3]*1E-1*4) ) ) {
 			cout << "Ekin: " << poly.update_ekin() << endl;
 			cout << "T: " << poly.calculate_temp() << endl;
 		}
-		thermostat.propagate();
+		thermostat->propagate();
 	}
 	
 	return 0;
 }
+
