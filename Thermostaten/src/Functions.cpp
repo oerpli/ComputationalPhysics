1f019cfddd1e33e92744c672b55262a8ec854f4d
paddy3 <paddytree3@gmail.com>
2014-11-18 13:15:27 +0100

    Funktionen für Histo und Stat hinzugefügt

--- Thermostaten/src/Functions.cpp
+++ Thermostaten/src/Functions.cpp
@@ -1,7 +1,17 @@
 #include "Functions.h"
 #include "Polymer.h" //redundant Thermostat
 #include "Monomer.h" //redundant Polymer
+
+#include "Stat.h"
+#include "Histo.h"
+#include <vector>
 #include <string>
+#include <sstream>
+
+#include<iostream>
+
+using namespace std;
+
 ostream& operator<<(ostream& os, const Monomer & mono) {
 	print_m(mono, os);
 	return os;
@@ -21,4 +31,28 @@ double set_param(double def, char *array[], int length, int pos) {
 	if (pos >= length) return def;
 	if (is_number(array[pos])) return stod(array[pos]);
 	return def;
+}
+
+void assign(istringstream &iss, const string &str) {
+	iss.clear();
+	iss.str(str);
+}
+
+ostream& operator<<(ostream& os, const Stat& stat) {
+	os << stat.mu << " " << stat.sigma << " ";
+	os << stat.min << " " << stat.max;
+return os;
+}
+
+bool histoLine(vector<Histo> &v_h, ostream &os) {
+	bool ret=false;
+	for (auto& h : v_h ) ret = ret | h.output(os);
+	return ret;
+}
+bool histoLine(vector<Histo> &v_h) {return histoLine(v_h,cout);}
+
+ostream& operator << (ostream& os , vector<Histo> &v_h) {
+	while ( histoLine(v_h,os) ) os << endl; 
+	for (auto& h : v_h) h.output_reset();
+return os;
 }
--- Thermostaten/src/Functions.h
+++ Thermostaten/src/Functions.h
@@ -3,6 +3,11 @@
 #include "Polymer.h" //redundant Thermostat
 #include "Monomer.h" //redundant Polymer
 
+#include "Stat.h"
+#include "Histo.h"
+#include <vector>
+#include <string>
+#include <sstream>
 
 #include<iostream>
 
@@ -13,3 +18,9 @@ ostream& operator<<(ostream& os, const Polymer & poly);
 
 bool is_number(const std::string &str);
 double set_param(double def, char *array[], int length, int pos);
+
+void assign(istringstream &iss, const string &str);
+ostream& operator<<(ostream& os, const Stat& stat);
+bool histoLine(vector<Histo> &v_h, ostream &os);
+bool histoLine(vector<Histo> &v_h);
+ostream& operator << (ostream& os , vector<Histo> &v_h);
