9f4f66ffcf8cdb003fd3e907d54858319d2db0fb
paddy3 <paddytree3@gmail.com>
2014-12-12 19:58:33 +0100

    Kombination boost::units mit Überladung Vektoroperation

---
+++ Harte_Kugeln/src/beispiel_c14.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//Bin noch nicht so vertraut mit Units, hab einfach mal einige reingeladen...
+#include <boost/units/cmath.hpp>
+#include <boost/units/io.hpp>
+#include <boost/units/systems/si.hpp>
+#include <boost/units/systems/si/io.hpp>
+#include <boost/units/systems/si/base.hpp>
+
+
+using namespace std;
+using namespace boost::units;
+using namespace boost::units::si;
+
+template<typename T1, typename T2>
+auto operator+ (const vector<T1>& v1, const vector<T2>& v2) {
+	vector< decltype( T1{} + T2{} ) > res( min(v1.size(), v2.size() ), 0);
+	for (int i=0; i<res.size(); ++i)
+		res[i] = v1[i] + v2[i];
+	return res;
+}
+
+template<typename T1, typename T2>
+auto operator* (const vector<T1>& v, const T2& s) {
+	vector< decltype( T1{} * T2{} ) > res( v.size(), 0);
+	for (int i=0; i<res.size(); ++i)
+		res[i] = v[i] * s;
+	return res;
+}
+
+template<typename T1, typename T2>
+auto operator* (const vector<T1>& v1, const vector<T2>& v2) {
+	decltype( T1{} * T2{} ) res=0;
+	auto size = min( v1.size(), v2.size() );
+
+	for (int i=0; i<size; ++i)
+		res += v1[i] * v2[i];
+	return res;
+}
+
+
+int main() {
+
+	force	N = newton;
+	length	m = meter;
+
+	quantity<force> F1(3*N);
+	quantity<length> l1(5*m);
+	auto E1(5*joule);
+
+	cout << "F1: " << F1 << '\n';
+	cout << "l1: " << l1 << '\n';
+	cout << "E1: " << E1 << '\n';
+	cout << "F1*l1-E1: " << F1 * l1 - E1 << '\n';
+
+
+	std::vector< quantity<length> >  vec_length(3);
+	vec_length[0] = l1;
+	vec_length[1] = 2 * m;
+	vec_length[2] = 10 * m;
+
+	std::vector< quantity<force> >  vec_newton(3);
+	for(int i=0; i<vec_newton.size(); ++i)
+		vec_newton[i] = 2 * i * newton;
+
+	cout << "Newton Vector:\n";
+	for(auto& el : vec_newton) cout << el << '\n';
+
+	cout << "Newton Vector * 5:\n";
+	quantity<dimensionless> scalar = 5;
+	auto vec_newton2 = vec_newton * scalar;
+	for(auto& el : vec_newton2) cout << el << '\n';
+
+	cout << "Length Vector:\n";
+	for(auto& el : vec_length) cout << el << '\n';
+
+	cout << "Newton Vec * Length Vec: ";
+	cout << vec_newton * vec_length << '\n';
+
+}
