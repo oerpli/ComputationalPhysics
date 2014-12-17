7b2a34e9e8a4a45bb8703727a7adab9b8f061de8
paddy3 <paddytree3@gmail.com>
2014-12-17 19:09:07 +0100

    Ekin hinzugefügt vec_vel nun private

--- Harte_Kugeln/lib/Kugel.h
+++ Harte_Kugeln/lib/Kugel.h
@@ -5,22 +5,36 @@
 #include <vector>
 #include "MatVec.h"
 
+#include <boost/units/cmath.hpp>
+#include <boost/units/pow.hpp>
 #include <boost/units/systems/si.hpp>
 #include <boost/units/systems/si/io.hpp>
 #include <boost/units/systems/si/base.hpp>
+#include <boost/units/derived_dimension.hpp>
 
+#include <boost/units/physical_dimensions/length.hpp>
+#include <boost/units/physical_dimensions/mass.hpp>
+#include <boost/units/physical_dimensions/time.hpp>
+
+using namespace boost::units;
+using namespace boost::units::si;
 
 template <unsigned DIM>
 class Kugel {
 	typedef boost::units::quantity< boost::units::si::length , double > length_type;
 	typedef boost::units::quantity< boost::units::si::velocity , double > velocity_type;
 	typedef boost::units::quantity< boost::units::si::mass , double > mass_type;
+	typedef boost::units::quantity< boost::units::si::energy , double > energy_type;
 
 	mass_type m_mass; //mass ist nun ein Datentyp
 	length_type m_diameter;
+	energy_type m_ekin;
+	MatVec<velocity_type, DIM> vec_vel; //velocity ist nun ein Datentyp
+
+	void update_ekin();
+
 public:
 	MatVec<length_type, DIM> vec_pos;
-	MatVec<velocity_type, DIM> vec_vel; //velocity ist nun ein Datentyp
 	//collision next_collision;
 
 	Kugel();
@@ -33,13 +47,17 @@ public:
 			swap(kugelA.vec_vel, kugelB.vec_vel); //swap von MatVec
 			swap(kugelA.m_mass, kugelB.m_mass); //units swap
 			swap(kugelA.m_diameter, kugelB.m_diameter); //units swap
+			swap(kugelA.m_ekin, kugelB.m_ekin);
 	}
+
+	void velocity(MatVec<velocity_type, DIM> vec);
+
+	auto ekin() const -> decltype(m_ekin);
 
 	std::ostream& print(std::ostream & os = std::cout) const;
 
 
 };
-
 
 #include "Kugel.tpp"
 
--- Harte_Kugeln/lib/Kugel.tpp
+++ Harte_Kugeln/lib/Kugel.tpp
@@ -1,23 +1,44 @@
 template <unsigned DIM>
 Kugel<DIM>::Kugel() :
 		m_mass(mass_type { }), m_diameter(length_type { })
-		, vec_pos{}, vec_vel{} {}
+		, m_ekin{}, vec_vel{}, vec_pos{} {}
 
 template <unsigned DIM>
 Kugel<DIM>::Kugel(const Kugel<DIM> & kugel) : m_mass{kugel.m_mass},
 	m_diameter{kugel.m_diameter},
-	vec_pos{kugel.vec_pos},
-	vec_vel{kugel.vec_vel} {}
+	m_ekin{kugel.m_ekin},
+	vec_vel{kugel.vec_vel},
+	vec_pos{kugel.vec_pos} {}
 
 template <unsigned DIM>
 Kugel<DIM>::Kugel(mass_type m, length_type d) :
 		m_mass { m }, m_diameter { d }
-		, vec_pos{}, vec_vel{} {}
+		, m_ekin{}, vec_vel{}, vec_pos{} {}
+
+template<unsigned DIM>
+void Kugel<DIM>::update_ekin() {
+	quantity < dose_equivalent > v2sum { }; //gibt sicher einen besseren Weg statt auto(decltype)
+	for (auto& el : vec_vel)
+		v2sum = v2sum + pow < 2 > (el);
+	m_ekin = 0.5 * m_mass * v2sum;
+}
+
+template<unsigned DIM>
+void Kugel<DIM>::velocity(MatVec<velocity_type, DIM> vec) {
+	swap(vec_vel, vec);
+	update_ekin();
+}
+
+template<unsigned DIM>
+auto Kugel<DIM>::ekin() const -> decltype(m_ekin) {
+	return m_ekin;
+}
 
 template <unsigned DIM>
 std::ostream& Kugel<DIM>::print(std::ostream & os) const {
 	os << "Mass: " << m_mass << '\n'; //Falls wir wieder zu langsam bei der Ausgabe sind
 	os << "Diameter: " << m_diameter << '\n';
+	os << "Ekin: " << m_ekin << '\n';
 	os << "Position: " << vec_pos << '\n';
 	os << "Velocity: " << vec_vel << '\n';
 	return os;
