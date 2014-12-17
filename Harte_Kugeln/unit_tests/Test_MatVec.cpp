2805c42865c835970237eb419049920e5703e43d
paddy3 <paddytree3@gmail.com>
2014-12-17 23:36:46 +0100

    Tests zu den Operationen

--- Harte_Kugeln/unit_tests/Test_MatVec.cpp
+++ Harte_Kugeln/unit_tests/Test_MatVec.cpp
@@ -6,12 +6,44 @@
 #include "MatVec.h"
 
 void empty_double_initialisation() {
-	MatVec<double, 3> vec;
+	MatVec<double, 3> vec { };
 	bool same{true};
 
 	for (auto& el : vec)
 		same &= el == 0;
 	ASSERTM("somewhere not the same", same);
+}
+
+void vektor_addition() {
+	MatVec<double, 3> vec1{1,2,3};
+	MatVec<double, 3> vec2{-1,-3,2};
+	MatVec<double, 3> res{0,-1,5};
+
+	ASSERTM("", vec1 + vec2 == res);
+}
+
+void vektor_subtraktion() {
+	MatVec<double, 3> vec1{1,2,3};
+	MatVec<double, 3> vec2{-1,-3,2};
+	MatVec<double, 3> res{2,5,1};
+
+	ASSERTM("", vec1 - vec2 == res);
+}
+
+void vektorElemente_SkalarAddition() {
+	MatVec<double, 3> vec{-1,-3,2};
+	MatVec<double, 3> res{1,-1,4};
+	double s = 2;
+
+	ASSERTM("", vec + s == res);
+}
+
+void vektorElemente_SkalarSubtraktion() {
+	MatVec<double, 3> vec{1,-3,2};
+	MatVec<double, 3> res{-1,-5,0};
+	double s = 2;
+
+	ASSERTM("", vec - s == res);
 }
 
 void test_norm2() {
@@ -31,12 +63,43 @@ void scalar_product() {
 	ASSERTM("",vec1*vec2 == -1);
 }
 
+void multiplikation_skalar() {
+	MatVec<double, 3> vec{1,-2,3};
+	MatVec<double, 3> res{-2,4,-6};
+	double skalar{-2};
+
+	ASSERTM("", vec * skalar == res);
+}
+
+void division_skalar() {
+	MatVec<double, 3> vec{-2,4,-6};
+	MatVec<double, 3> res{1,-2,3};
+	double skalar{-2};
+
+	ASSERTM("", vec / skalar == res);
+}
+
+void make_negative() {
+	MatVec<double, 3> vec{-2,4,-6};
+	MatVec<double, 3> res{2,-4,6};
+
+	ASSERTM("", -vec == res);
+}
+
+
 void runAllTests(int argc, char const *argv[]){
 	cute::suite s;
 	s.push_back(CUTE(empty_double_initialisation));
 	s.push_back(CUTE(scalar_product));
 	s.push_back(CUTE(test_norm2));
 	s.push_back(CUTE(test_norm));
+	s.push_back(CUTE(multiplikation_skalar));
+	s.push_back(CUTE(division_skalar));
+	s.push_back(CUTE(make_negative));
+	s.push_back(CUTE(vektor_addition));
+	s.push_back(CUTE(vektor_subtraktion));
+	s.push_back(CUTE(vektorElemente_SkalarAddition));
+	s.push_back(CUTE(vektorElemente_SkalarSubtraktion));
 	//TODO add your test here
 	cute::xml_file_opener xmlfile(argc,argv);
 	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
