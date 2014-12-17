332ea46f82fae871dd49a34bd71235de2ed95b37
paddy3 <paddytree3@gmail.com>
2014-12-17 01:21:08 +0100

    Ersetzten der Templates LengthType ... mit den entsprechenden Einheiten Eingliedern von MatVec

--- Harte_Kugeln/src/Kugel.cpp
+++ Harte_Kugeln/src/Kugel.cpp
@@ -1,31 +1,23 @@
-template <typename LengthType, typename VelocityType, typename MassType, int DIM>
-Kugel<LengthType, VelocityType, MassType, DIM>::Kugel() : mass(MassType()), diameter(LengthType()) {
-	for (int i = 0; i < DIM; i++) {
-		position.push_back(LengthType());
-		velocity.push_back(VelocityType());
-	}
-}
+template <unsigned DIM>
+Kugel<DIM>::Kugel() : m_mass(quantity<mass>{}), m_diameter(quantity<length>{})
+					, vec_pos{}, vec_vel{} {}
 
-template <typename LengthType, typename VelocityType, typename MassType, int DIM>
-Kugel<LengthType, VelocityType, MassType, DIM>::Kugel(const Kugel<LengthType, VelocityType, MassType, DIM> & kugel) : mass{kugel.mass},
-	diameter{kugel.diameter},
-	position{kugel.position},
-	velocity{kugel.velocity} {}
+template <unsigned DIM>
+Kugel<DIM>::Kugel(const Kugel<DIM> & kugel) : m_mass{kugel.m_mass},
+	m_diameter{kugel.m_diameter},
+	vec_pos{kugel.vec_pos},
+	vec_vel{kugel.vec_vel} {}
 
-template <typename LengthType, typename VelocityType, typename MassType, int DIM>
-Kugel<LengthType, VelocityType, MassType, DIM>::Kugel(MassType m, LengthType d) : mass(m), diameter(d) {
-	for (int i = 0; i < DIM; i++) {
-		position.push_back(LengthType());
-		velocity.push_back(VelocityType());
-	}
-}
+template <unsigned DIM>
+Kugel<DIM>::Kugel(quantity<mass> m, quantity<length> d) : m_mass{m}, m_diameter{d}
+														, vec_pos{}, vec_vel{} {}
 
-template <typename LengthType, typename VelocityType, typename MassType, int DIM>
-std::ostream& Kugel<LengthType, VelocityType, MassType, DIM>::print(std::ostream & os) const {
-	os << mass << " ";
-	os << diameter << " ";
-	os << position << " ";
-	os << velocity << std::endl;
+template <unsigned DIM>
+std::ostream& Kugel<DIM>::print(std::ostream & os) const {
+	os << "Mass: " << m_mass << '\n'; //Falls wir wieder zu langsam bei der Ausgabe sind
+	os << "Diameter: " << m_diameter << '\n';
+	os << "Position: " << vec_pos << '\n';
+	os << "Velocity: " << vec_vel << '\n';
 	return os;
 }
 
--- Harte_Kugeln/src/Kugel.h
+++ Harte_Kugeln/src/Kugel.h
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <vector>
+#include "MatVec.h"
 
-template <typename LengthType, typename VelocityType, typename MassType, int DIM>
+#include <boost/units/systems/si.hpp>
+#include <boost/units/systems/si/io.hpp>
+#include <boost/units/systems/si/base.hpp>
+
+using namespace boost::units;
+using namespace boost::units::si;
+
+template <unsigned DIM>
 class Kugel {
+	quantity<mass> m_mass; //mass ist nun ein Datentyp
+	quantity<length> m_diameter;
 public:
-	MassType mass;
-	LengthType diameter;
-	std::vector<LengthType> position;
-	std::vector<VelocityType> velocity;
+	MatVec<quantity<length>,DIM> vec_pos;
+	MatVec<quantity<velocity>,DIM> vec_vel; //velocity ist nun ein Datentyp
 	//collision next_collision;
 
 	Kugel();
-	Kugel(const Kugel<LengthType, VelocityType, MassType, DIM> & kugel);
-	Kugel(MassType m, LengthType d);
+	Kugel(const Kugel<DIM> & kugel);
+	Kugel(quantity<mass> m_mass, quantity<length> d);
 
-	friend void swap(Kugel<LengthType, VelocityType, MassType, DIM>& kugelA, Kugel<LengthType, VelocityType, MassType, DIM>& kugelB) {
-			std::swap(kugelA.position, kugelB.position);
-			std::swap(kugelA.velocity, kugelB.velocity);
+	friend void swap(Kugel<DIM>& kugelA, Kugel<DIM>& kugelB) {
+			swap(kugelA.vec_pos, kugelB.vec_pos); //swap von MatVec
+			swap(kugelA.vec_vel, kugelB.vec_vel); //swap von MatVec
 			std::swap(kugelA.mass, kugelB.mass);
 			std::swap(kugelA.diameter, kugelB.diameter);
 	}
