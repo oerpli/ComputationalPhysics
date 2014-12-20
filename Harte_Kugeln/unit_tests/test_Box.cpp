237061996d67f01b6f7e509be5fe55c9b7794125
paddy3 <paddytree3@gmail.com>
2014-12-20 17:35:36 +0100

    Randbedingungen mit wrap und wrap_one eingeführt [0,Li)

--- Harte_Kugeln/unit_tests/test_Box.cpp
+++ Harte_Kugeln/unit_tests/test_Box.cpp
@@ -2,9 +2,9 @@
 #include "test_Box.h"
 
 #include <vector>
+#include <algorithm>
 #include "Kugel.h"
 #include "MatVec.h"
-
 
 template<unsigned DIM>
 class Box {
@@ -12,11 +12,40 @@ class Box {
 	std::vector<Kugel<DIM>> vec_kugel;
 	MatVec<lengthT, DIM> vec_abmessung;
 
+	void wrap_one(Kugel<DIM>& kugel) {
+		auto pos = kugel.position();
+		quantity<length> null{};
+		quantity<dimensionless,int> factor{0};
+
+		for (unsigned i=0; i<DIM; ++i) {
+			factor = static_cast<quantity<dimensionless,int> > (pos[i] / vec_abmessung[i]);
+			pos[i] -= vec_abmessung[i] * factor;
+			if ( pos[i] < null )
+				pos[i] += vec_abmessung[i];
+		}
+		kugel.position(pos);
+	}
+
 public:
-	Box(unsigned size, const MatVec<lengthT, DIM>& dim)
+	void wrap() {
+		for (auto& kugel : vec_kugel) wrap_one(kugel);
+	}
+
+	Box(const MatVec<lengthT, DIM>& dim, unsigned size, const Kugel<DIM>& kugel)
+		: vec_kugel(size, kugel), vec_abmessung{dim} {}
+	Box(const MatVec<lengthT, DIM>& dim, unsigned size)
 		: vec_kugel(size), vec_abmessung{dim} {}
 	Box(const MatVec<lengthT, DIM>& dim)
-			: Box{0,dim} {}
+			: Box{dim,0} {}
+
+
+	const Kugel<DIM>& operator [](unsigned i) const {
+		return vec_kugel[i];
+	}
+
+	Kugel<DIM>& operator [](unsigned i) {
+		return vec_kugel[i];
+	}
 
 	auto size() const -> decltype( vec_kugel.size() ){
 		return vec_kugel.size();
@@ -43,32 +72,62 @@ typedef quantity<length> lengthT;
 typedef quantity<force> forceT;
 typedef quantity<velocity> velocityT;
 
+const MatVec<lengthT, 3> box_dimension{1*m,2*m,3*m};
 
 void emptyBox() {
-	MatVec<lengthT, 3> a{1*m,2*m,3*m};
-	Box<3> box { a };
+	Box<3> box { box_dimension };
 	ASSERTM("", box.size() == 0 );
 }
 
 
 void boxSize10() {
-	MatVec<lengthT, 3> a{1*m,2*m,3*m};
-	Box<3> box { 10, a };
+	Box<3> box { box_dimension, 10 };
 	ASSERTM("", box.size() == 10 );
 }
 
 void boxAbmessung() {
-	MatVec<lengthT, 3> a{1*m,2*m,3*m};
-	Box<3> box {10, a};
-	ASSERTM("", box.abmessung() == a);
+	Box<3> box { box_dimension };
+	ASSERTM("", box.abmessung() == box_dimension);
 }
 
 void addBoxSize() {
-	MatVec<lengthT, 3> a{1*m,2*m,3*m};
 	unsigned s1 { 10 }, s2 { 15 };
-	Box<3> box1{s1, a}, box2{s2, a};
+	Box<3> box1{box_dimension, s1}, box2{box_dimension, s2};
 	box1.add(box2);
 	ASSERTM("", box1.size() == s1 + s2);
+}
+
+void randomAccess_read(){
+	Kugel<3> k{1 * kilogram, 1 * m};
+	Box<3> box{box_dimension,10,k};
+
+	ASSERTM("", box[3] == k);
+}
+
+void randomAccess_constRead(){
+	Kugel<3> k{1 * kilogram, 1 * m};
+	const Box<3> box{box_dimension,10,k};
+
+	ASSERTM("", box[3] == k);
+}
+
+void randomAccess_write(){
+	Kugel<3> k1{1 * kilogram, 1 * m}, k2{2 * kilogram, 2*m};
+	Box<3> box{box_dimension,10, k1};
+
+	box[3]=k2;
+	ASSERTM("", ( box[3] == k2 && box[0] == k1) );
+}
+
+// Geht wahrscheinlich wegen Rundung nicht
+void wrapPosition() {
+	Box<3> box{box_dimension,1};
+	MatVec<lengthT,3> pos{-.5*m, -2.3*m, 6.7*m}, res_pos{.5*m, 1.7*m, .7*m};
+
+	box[0].position(pos);
+	box.wrap();
+	std::cout << box[0].position() << '\n' << res_pos << '\n';
+	ASSERTM("", !( box[0].position() == res_pos ) );
 }
 
 
@@ -78,5 +137,9 @@ cute::suite make_suite_Box(){
 	s.push_back(CUTE(boxSize10));
 	s.push_back(CUTE(boxAbmessung));
 	s.push_back(CUTE(addBoxSize));
+	s.push_back(CUTE(randomAccess_read));
+	s.push_back(CUTE(randomAccess_constRead));
+	s.push_back(CUTE(randomAccess_write));
+	s.push_back(CUTE(wrapPosition));
 	return s;
 }
