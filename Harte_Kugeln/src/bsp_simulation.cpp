c1f7ae239dc98c54c751da60bdf1e02cc3acdd87
paddy3 <paddytree3@gmail.com>
2015-01-07 05:16:43 +0100

    umschreiben der Auswerteintervalle

--- Harte_Kugeln/src/bsp_simulation.cpp
+++ Harte_Kugeln/src/bsp_simulation.cpp
@@ -29,9 +29,7 @@ int main() {
 
 	//TODO: Warmlauf einfügen
 
-	timeT ausw_time{}, d_ausw_time{13*s};
-	timeT next_ausw_time = ausw_time + d_ausw_time;
-	timeT d_time{}, zero_time{};
+	timeT ausw_t_step{13*s}, ausw_t_next{ausw_t_step};
 
 	box(vec_unary,vec_binary);
 	vec_unary.print_result(cout);
@@ -40,12 +38,14 @@ int main() {
 /*
 	unsigned max_collisions{100}; //TODO: kann abhängig von Eingabe sein
 	for (unsigned i = 0; i < max_collision; ++i) {
-		box.next_collision(); //update next_collision_time in absolute_time
-
-		while ( (d_time = box.next_collision_time() - next_ausw_time) > zero_time) {
-			box.fast_forward(d_time);
+		while ( ausw_t_next < box.next_event() ) {
+			box.fast_forward(ausw_t_next);
+			ausw_t_next = ausw_t_step;
 			box(vec_unitaryF, vec_binaryF);
 		}
+		ausw_t_next -= box.next_event();
+		box.next_collision(); //update next_collision_time in absolute_time
+
 	}
 */
 }
