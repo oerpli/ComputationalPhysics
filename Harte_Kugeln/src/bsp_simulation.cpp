9211e29b3711d41ae0e066a6ad3b3b8ee2c2c3c6
paddy3 <paddytree3@gmail.com>
2015-01-07 02:09:48 +0100

    Verwendung der box(unary,binary) Funktion

--- Harte_Kugeln/src/bsp_simulation.cpp
+++ Harte_Kugeln/src/bsp_simulation.cpp
@@ -14,26 +14,29 @@ using namespace std;
 
 int main() {
 	const unsigned DIM {3}; //TODO: kann abhängig von Eingabe sein
-	AuswertVec<Kugel<DIM>> fvec_func;
+	AuswertVec<Kugel<DIM>> vec_unary;
+	AuswertVec<Kugel<DIM>,Kugel<DIM>> vec_binary;
 
 	{//TODO: kann abhängig von Eingabe sein
-		fvec_func.push_back( new auswertung_bsp_average_vel<DIM> );
-		fvec_func.push_back( new auswertung_bsp_average_vel<DIM> );
+		vec_unary.push_back( new auswertung_bsp_average_vel<DIM> );
+		vec_unary.push_back( new auswertung_bsp_average_vel<DIM> );
 	}
 	MatVec<lengthT,DIM> box_size{10*m, 10*m, 10*m}; //TODO: kann abhängig von Eingabe sein
 	Kugel<DIM> kugel1{.5 * kg, .5 * m}; //TODO: kann abhängig von Eingabe sein
-	Box<DIM> box{box_size, 10}; //TODO: kann abhängig von Eingabe sein
+	MatVec<velocityT,DIM> vel{14*mps};
+	kugel1.velocity(vel);
+	Box<DIM> box{box_size, 10, kugel1}; //TODO: kann abhängig von Eingabe sein
 
 	//TODO: Warmlauf einfügen
 
 	timeT ausw_time{}, d_ausw_time{13*s};
 	timeT next_ausw_time = ausw_time + d_ausw_time;
 	timeT d_time{}, zero_time{};
-	MatVec<velocityT,DIM> vel{14*mps};
-	kugel1.velocity(vel);
 
-	fvec_func(kugel1);
-	fvec_func.print_result(cout);
+	box(vec_unary,vec_binary);
+	vec_unary.print_result(cout);
+	cout << '\n';
+	vec_binary.print_result(cout);
 /*
 	unsigned max_collisions{100}; //TODO: kann abhängig von Eingabe sein
 	for (unsigned i = 0; i < max_collision; ++i) {
