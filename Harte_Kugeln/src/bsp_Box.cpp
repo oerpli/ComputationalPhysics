3f902e4eb6b29766b4d3da77b6062ac865494d37
paddy3 <paddytree3@gmail.com>
2015-01-07 14:39:50 +0100

    bsp_Box update

--- Harte_Kugeln/src/bsp_Box.cpp
+++ Harte_Kugeln/src/bsp_Box.cpp
@@ -7,10 +7,11 @@ using namespace std;
 int main() {
 	const unsigned DIM {3};
 	MatVec<lengthT,DIM> abmessung{2*m,2*m,2*m};
-	Box<DIM> box{abmessung,10};
+	Kugel<DIM> kugel1{1*kg, .1*m};
+
+	Box<DIM> box{abmessung, 10, kugel1};
 
 	cout << "Start\n" << box << '\n';
-	box.initiate();
+	if(! box.initiate()) return 1;
 	cout << "after Init\n" << box << '\n';
-
 }
