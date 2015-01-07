fc6717b3255941ea9765f97516684f0f5cba7487
paddy3 <paddytree3@gmail.com>
2015-01-07 01:49:38 +0100

    Überklasse von der Auswertefunktionen erben

---
+++ Harte_Kugeln/src_sonstiges/Auswert.h
@@ -0,0 +1,15 @@
+#ifndef AUSWERT_H_
+#define AUSWERT_H_
+
+#include <ostream>
+
+template<class ... Args>
+class Auswert {
+public:
+	virtual ~Auswert(){}
+	virtual void operator() (const Args& ... args){std::cout << "Auswert.operator()\n";}
+	virtual double value(){return -1.0;}
+	virtual std::ostream& print_result(std::ostream& os) {return os;}
+};
+
+#endif //AUSWERT_H_
