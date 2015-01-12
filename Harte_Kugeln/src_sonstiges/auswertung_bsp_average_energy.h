03b8895367b4118566fee344bd40c0771b587e3f
paddy3 <paddytree3@gmail.com>
2015-01-12 05:04:07 +0100

    average_ekin Auswerteklasse erstellt

---
+++ Harte_Kugeln/src_sonstiges/auswertung_bsp_average_energy.h
@@ -0,0 +1,25 @@
+#ifndef AUSWERTUNG_BSP_AVERAGE_ENERGY_H_
+#define AUSWERTUNG_BSP_AVERAGE_ENERGY_H_
+
+#include "Auswert.h"
+#include "Kugel.h"
+
+template<unsigned DIM>
+class auswertung_bsp_average_energy : public Auswert<Kugel<DIM>> {
+public:
+	energyT sum_energy;
+	unsigned count;
+
+public:
+	void operator() (const Kugel<DIM>& k) {
+		sum_energy += k.ekin();
+		count++;
+	}
+	double value() { return (sum_energy / (N*m)) / count; }
+	std::ostream& print_result(std::ostream& os) {
+		os << value() << " ";
+		return os;
+	}
+};
+
+#endif //AUSWERTUNG_BSP_AVERAGE_ENERGY_H_
