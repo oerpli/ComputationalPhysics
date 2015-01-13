cb4426420f78a87967cf40d0343babc650356f3d
paddy3 <paddytree3@gmail.com>
2015-01-13 03:06:55 +0100

    bsp_collision_update.cpp nun kleines Newtonpendel

--- Harte_Kugeln/src/bsp_collision_update.cpp
+++ Harte_Kugeln/src/bsp_collision_update.cpp
@@ -10,40 +10,24 @@ using namespace std;
 
 int main () {
 	const unsigned dim{3};
+	MatVec<lengthT,dim> pos{8 *m, 0 *m, 0 *m};
+	MatVec<velocityT,dim> vel {1 *mps, 0 *mps, 0 *mps};
+	Box<3> box{MatVec<lengthT,dim>{10*m}, 2, Kugel<dim>{4 *kg, 1 *m}};
+	timeT coll_time{};
 
-	const MatVec<lengthT, 3> box_dimension{10*m,10*m,10*m};
+	box[0].position(pos);
+	box[1].velocity(vel);
+	box.initiate();
 
-	Box<3> box { box_dimension , 2};
-
-	lengthT dia= 1* m;
-	MatVec<velocityT, dim> v_vel{1*mps, 0*mps, 0*mps};
-	MatVec<lengthT, dim> v_pos{0*m, 0*m, 0*m};
-	MatVec<lengthT, dim> v_pos2{4*m, 0*m, 0*m};
-
-	v_pos = v_pos * 0.5;
-
-	Kugel<dim> kugel1{1 * kg, dia}, kugel2{1 * kg, dia};
-
-	kugel1.position(v_pos);
-	kugel2.position(v_pos2);
-
-	kugel1.velocity(v_vel);
-	kugel2.velocity(-v_vel);
-
-	box[0]=kugel1;
-	box[1]=kugel2;
-
-	box[0].print();
-	box[1].print();
-
-	box.init_next_collision();
-	std::cout << "\nafter collision" << std::endl;
-	box[0].print();
-	box[1].print();
-
-	box.next_collision();
-	std::cout << "\nafter next collision" << std::endl;
-	box[0].print();
-	box[1].print();
+	cout << box;
+	coll_time = box.collide();
+	cout << coll_time << '\n';
+	cout << box;
+	coll_time = box.collide();
+	cout << coll_time << '\n';
+	cout << box;
+	coll_time = box.collide();
+	cout << coll_time << '\n';
+	cout << box;
 
 }
