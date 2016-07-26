fe6618919d8cfeb9a7b632dfa6d9912a4f47d728
Maud <maud.formanek@univie.ac.at>
2016-07-26 13:47:24 +0200

    updated protokoll + dependant files

--- Harte_Kugeln/results/plot_results.py
+++ Harte_Kugeln/results/plot_results.py
@@ -43,8 +43,10 @@ name_image_pair = " "
 
 name_output = "eos_"+str(parameter)+".dat"
 name_output_diffusion = "diffusion_"+str(parameter)+".dat"
+name_output_collision = "collision_"+str(parameter)+".dat"
 f_output = open(name_output, 'w')
 f_output_diffusion = open(name_output_diffusion, 'w')
+f_output_collision = open(name_output_collision, 'w')
 search_string = "./Pair_distribution_"+str(parameter)+"*.dat"
 print search_string
 
@@ -60,7 +62,8 @@ for file in sorted(glob.glob(search_string), key =sort_by_rho):
     diffusion_constant = float(data[4])
     f_output.write("%f" %rho + " " + "%f " %(1.+2.*np.pi*g*rho/3.) + "%f" %eos_momentum + "\n")
     f_output_diffusion.write("%f" %rho + " " + "%f " %diffusion_constant + "\n")
-
+    f_output_collision.write("%f" %rho + " " + "%f " %(4.*np.pi*rho*np.sqrt(np.pi)*g)+"\n")
+ 
 # collisions auswertung noch experimentell :P 
     coll_file = file.replace("Pair_distribution", "Coll_time")
     collx, colly = np.loadtxt(coll_file, unpack=True)
@@ -107,7 +110,7 @@ for file in sorted(glob.glob(search_string), key =sort_by_rho):
         plot_data_g += " , "
     else: 
         gnu( 'set xrange [1:3.5]' )
-        gnu( 'set xlabel "r [m]"' )
+        gnu( 'set xlabel "r"' )
         gnu( 'set ylabel "g(r)"' ) 
         gnu( 'set term x11 ' + str(number_of_files) ) 
         gnu( 'set output' )
@@ -115,7 +118,7 @@ for file in sorted(glob.glob(search_string), key =sort_by_rho):
         plot_data_g = plot_data_g.replace("t 'rho","t '$\\rho$")
         print plot_data_g
         gnu( 'set terminal epslatex size 15cm,12cm color colortext')
-        gnu( 'set xlabel "$r [\text{m}]$"' )
+        gnu( 'set xlabel "$r$"' )
         gnu( 'set ylabel "$g(r)$"' )          
        # gnu( 'set terminal png large size 1024,768' )
  #       name_image = file.partition(".dat")[0] + ".png" # changed .png to .tex
@@ -147,35 +150,44 @@ for file in sorted(glob.glob(search_string), key =sort_by_rho):
     #plot autocorrelation
     plot_data = "plot '" + value_file + "' u 1:2 w l t 'rho = " + str(rho)
 #    gnu( 'set title "Velocity autocorrelation function"' )
-    gnu( 'set xlabel "t [s]"' )
-    gnu( 'set ylabel "<v(0)v(t)> [m^2/s^2]"' ) 
+#    gnu( 'set xrange [0:0.5]' )
+    gnu( 'set xlabel "t"' )
+    gnu( 'set ylabel "<v(0)v(t)>"' ) 
     gnu( 'set term x11 ' + str(number_of_files+1) ) 
     gnu( 'set output' )
     gnu( plot_data )
-    gnu( 'set terminal png large size 1024,768' )
+    gnu( 'set terminal epslatex size 15cm,12cm color colortext')
+    gnu( 'set xlabel "$t $"' )
+    gnu( 'set ylabel "$\\langle \\vec{v}(0) \\vec{v}(t)\\rangle$"' )
     name_image = value_file.replace("values", "autocorrelation") 
-    name_image = name_image.replace(".dat", ".png")
+    name_image = name_image.replace(".dat", ".tex")
     gnu( 'set output "' + name_image + '"' )
     gnu( 'repl' )
     print "Das Bild '" + name_image + "' wurde erstellt.\n"     
-
+    
+    gnu( 'set autoscale x')
   
 f_output.close()
 f_output_diffusion.close()
   
 gnu( 'pi = 3.141593' )
-gnu( 'f(x) = 1 + (x*pi/6.0 + (x*pi/6.0)**2 - (x*pi/6.0)**3)/((1-(x*pi/6.0))**3) ' ) 
+gnu( 'f(x) = x <= 0.945 ? 1 + (x*pi/6.0 + (x*pi/6.0)**2 - (x*pi/6.0)**3)/((1-(x*pi/6.0))**3) : 1/0 ' ) 
+gnu( 'g(x) = 1 + (3.68584*(x*pi/6.0)/(1-2.5848*(x*pi/6.0) + 1.9499*((x*pi/6.0)**2) - 0.172284*((x*pi/6.0)**3)-0.16012*((x*pi/6.0)**4))) ' )
 #gnu( 'set title "Equation of state"' )
-gnu( 'set xlabel "rho [1/m^3]"' )
+gnu( 'set xlabel "rho "' )
 gnu( 'set ylabel "p/(rho*k*T)"' )
+gnu( 'set key Left reverse top left') 
 #gnu( 'set xrange[0:1.4]' )
 gnu( 'set term x11 ' + str(number_of_files+3) ) 
 gnu( 'set output' )
-plot_data = "plot '" + name_output + "' u 1:($2/2.) title 'aus Paarverteilungsfunktion', '" + name_output + "' u 1:($3/2.) title 'aus Impulsübertrag', f(x) title 'Theorie'" 
+gnu( 'set arrow 1 from 0.943,0 to 0.943,20 lt 2 lc rgb "black" lw 0.5 nohead' )
+gnu( 'set arrow 2 from 1.041,0 to 1.041,20 lt 2 lc rgb "black" lw 0.5 nohead' )
+plot_data = "plot '" + name_output + "' u 1:($2/2.) title 'aus Paarverteilungsfunktion', '" + name_output + "' u 1:($3/2.) title 'aus Impulsübertrag', f(x) title 'Carnahan & Starling'" 
 gnu( plot_data )
 gnu( 'set terminal epslatex size 15cm,12cm color colortext')
-gnu( 'set xlabel "$\\rho [m^{-3}]$"' )
+gnu( 'set xlabel "$\\rho $"' )
 gnu( 'set ylabel "$p/(\\rho k T)$"' )
+
 #gnu( 'set terminal png large size 1024,768' )
 #name_image = name_output.partition(".dat")[0] + ".tex"
 name_image = "eos.tex"
@@ -184,17 +196,21 @@ gnu( 'repl' )
 print "Das Bild '" + name_image + "' wurde erstellt.\n" 
 
 
-gnu( 'f(x) = (3./8.)*'+str(radius*2./(mass**(1./2.)))+'*(1./(pi**(1./2.)))*(1/x)*(1-x/1.09)*(1+x**2*(0.4-0.83*x**2))' )
+gnu( 'f(x) = x <= 0.945 ? (3./8.)*'+str(radius*2./(mass**(1./2.)))+'*(1./(pi**(1./2.)))*(1/x)*(1-x/1.09)*(1+x**2*(0.4-0.83*x**2)) : 1/0' )
 #gnu( 'set title "Diffusion constant"' )
-gnu( 'set ylabel "D [m^2/s]"' )
+gnu( 'set ylabel "D"' )
+gnu( 'set key default') 
+gnu( 'set yrange [-0.1:]' )
 gnu( 'set term x11 ' + str(number_of_files+4) ) 
 gnu( 'set output' )
+gnu( 'set arrow 1 from 0.943,-0.1 to 0.943,0.9 lt 2 lc rgb "black" lw 0.5 nohead' )
+gnu( 'set arrow 2 from 1.041,-0.1 to 1.041,0.9 lt 2 lc rgb "black" lw 0.5 nohead' )
 plot_data = "plot '" + name_output_diffusion + "' u 1:2 title 'Simulation', f(x) title 'Theorie'" 
 gnu( plot_data )
 
 gnu( 'set terminal epslatex size 15cm, 12cm color colortext' )
-gnu( 'set ylabel "$D [\\text{m}^2 s^{-1}]$"' )
-gnu( 'set xlabel "$\\rho [m^{-3}]$"' )
+gnu( 'set ylabel "$D$"' )
+gnu( 'set xlabel "$\\rho $"' )
 #name_image = name_output_diffusion.partition(".dat")[0] + ".tex"
 name_image = "diffusion.tex"
 gnu( 'set output "' + name_image + '"' )
